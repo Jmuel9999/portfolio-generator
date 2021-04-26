a57545c7c0c9d5a9feb135c825a0fc99660eb962
jasonmueller <jmuel9999@gmail.com>
2021-04-26 18:29:58 -0500

    Finished with feature/generate-webpage

--- app.js
+++ app.js
@@ -1,20 +1,105 @@
-// Leaves an empty array for input
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+// This calls upon the function 'generatePage' from page-template.js to be used in THIS code
+const generatePage = require('./src/page-template');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-   // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2);
 
-  console.log('================');
+const [name, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length)
+// // These two variables can also be shown...
+//   // const name = profileDataArgs[0];
+//   // const github = profileDataArgs[1];
+// // Like this
+// const [name, github] = profileDataArgs;
+// // Allows for the usage of 'fs' in my code
+// const fs = require('fs');
+
+// // Parenthesis are holding the place of where a parameter would have been, but no need for this function since we are only using one parameter
+// const generatePage = (userName, githubName) => {
+//   return `
+//     <!DOCTYPE html> 
+//     <html lang="en"> 
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <title>Portfolio Demo</title>
+//     </head>
+
+//     <body>
+//       <h1>${name}</h1>
+//       <h2><a href="https://github.com/${github}">Github</a></h2>
+//     </body>
+//     </html>
+//   `;
+// };
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   //Error statement
+//   if (err) throw err;
+
+//   //Success statement
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Leaves an empty array for input
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//    // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach((profileItem) => {
+//     console.log(profileItem)
+//   });
+// };
+
+// printProfileData(profileDataArgs);
